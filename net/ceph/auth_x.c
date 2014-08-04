c67c4936efe53dec19e53697ce7b151437410e02
Sage Weil <sage@redhat.com>
2014-08-04 07:07:49 -0700

    libceph: do not hard code max auth ticket len
    We kmalloc the temporary buffer anyway; do that a bit later so that we can
    allocate one as big as we need it.
    
    CC: stable@vger.kernel.org
    Signed-off-by: Sage Weil <sage@redhat.com>

--- net/ceph/auth_x.c
+++ net/ceph/auth_x.c
@@ -13,8 +13,6 @@
 #include "auth_x.h"
 #include "auth_x_protocol.h"
 
-#define TEMP_TICKET_BUF_LEN	256
-
 static void ceph_x_validate_tickets(struct ceph_auth_client *ac, int *pneed);
 
 static int ceph_x_is_authenticated(struct ceph_auth_client *ac)
@@ -64,7 +62,7 @@ static int ceph_x_encrypt(struct ceph_crypto_key *secret,
 }
 
 static int ceph_x_decrypt(struct ceph_crypto_key *secret,
-			  void **p, void *end, void *obuf, size_t olen)
+			  void **p, void *end, void **obuf, size_t olen)
 {
 	struct ceph_x_encrypt_header head;
 	size_t head_len = sizeof(head);
@@ -75,7 +73,14 @@ static int ceph_x_decrypt(struct ceph_crypto_key *secret,
 		return -EINVAL;
 
 	dout("ceph_x_decrypt len %d\n", len);
-	ret = ceph_decrypt2(secret, &head, &head_len, obuf, &olen,
+	if (*obuf == NULL) {
+		*obuf = kmalloc(len, GFP_NOFS);
+		if (!*obuf)
+			return -ENOMEM;
+		olen = len;
+	}
+
+	ret = ceph_decrypt2(secret, &head, &head_len, *obuf, &olen,
 			    *p, len);
 	if (ret)
 		return ret;
@@ -137,18 +142,9 @@ static int ceph_x_proc_ticket_reply(struct ceph_auth_client *ac,
 	int num;
 	void *p = buf;
 	int ret;
-	char *dbuf;
-	char *ticket_buf;
+	char *dbuf = NULL;
+	char *ticket_buf = NULL;
 	u8 reply_struct_v;
-
-	dbuf = kmalloc(TEMP_TICKET_BUF_LEN, GFP_NOFS);
-	if (!dbuf)
-		return -ENOMEM;
-
-	ret = -ENOMEM;
-	ticket_buf = kmalloc(TEMP_TICKET_BUF_LEN, GFP_NOFS);
-	if (!ticket_buf)
-		goto out_dbuf;
 
 	ceph_decode_need(&p, end, 1 + sizeof(u32), bad);
 	reply_struct_v = ceph_decode_8(&p);
@@ -188,8 +184,7 @@ static int ceph_x_proc_ticket_reply(struct ceph_auth_client *ac,
 		}
 
 		/* blob for me */
-		dlen = ceph_x_decrypt(secret, &p, end, dbuf,
-				      TEMP_TICKET_BUF_LEN);
+		dlen = ceph_x_decrypt(secret, &p, end, &dbuf, 0);
 		if (dlen <= 0) {
 			ret = dlen;
 			goto out;
@@ -220,8 +215,8 @@ static int ceph_x_proc_ticket_reply(struct ceph_auth_client *ac,
 		if (is_enc) {
 			/* encrypted */
 			dout(" encrypted ticket\n");
-			dlen = ceph_x_decrypt(&old_key, &p, end, ticket_buf,
-					      TEMP_TICKET_BUF_LEN);
+			dlen = ceph_x_decrypt(&old_key, &p, end, &ticket_buf,
+					      0);
 			if (dlen < 0) {
 				ret = dlen;
 				goto out;
@@ -583,13 +578,14 @@ static int ceph_x_verify_authorizer_reply(struct ceph_auth_client *ac,
 	struct ceph_x_ticket_handler *th;
 	int ret = 0;
 	struct ceph_x_authorize_reply reply;
+	struct ceph_x_authorize_reply *preply = &reply;
 	void *p = au->reply_buf;
 	void *end = p + sizeof(au->reply_buf);
 
 	th = get_ticket_handler(ac, au->service);
 	if (IS_ERR(th))
 		return PTR_ERR(th);
-	ret = ceph_x_decrypt(&th->session_key, &p, end, &reply, sizeof(reply));
+	ret = ceph_x_decrypt(&th->session_key, &p, end, &preply, sizeof(reply));
 	if (ret < 0)
 		return ret;
 	if (ret != sizeof(reply))
