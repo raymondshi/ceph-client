20081dcc2cc09b755381e4436c38a2358bbdde1d
Samuel Just <sam.just@inktank.com>
2014-09-23 11:26:15 -0700

    Revert "btrfs: Use right extent length when inserting overlap extent map."
    This reverts commit 51f395ad4058883e4273b02fdebe98072dbdc0d2.

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -6154,14 +6154,14 @@ out_fail:
 static int merge_extent_mapping(struct extent_map_tree *em_tree,
 				struct extent_map *existing,
 				struct extent_map *em,
-				u64 map_start)
+				u64 map_start, u64 map_len)
 {
 	u64 start_diff;
 
 	BUG_ON(map_start < em->start || map_start >= extent_map_end(em));
 	start_diff = map_start - em->start;
 	em->start = map_start;
-	em->len = existing->start - em->start;
+	em->len = map_len;
 	if (em->block_start < EXTENT_MAP_LAST_BYTE &&
 	    !test_bit(EXTENT_FLAG_COMPRESSED, &em->flags)) {
 		em->block_start += start_diff;
@@ -6449,7 +6449,8 @@ insert:
 							 em->len);
 			if (existing) {
 				err = merge_extent_mapping(em_tree, existing,
-							   em, start);
+							   em, start,
+							   root->sectorsize);
 				free_extent_map(existing);
 				if (err) {
 					free_extent_map(em);
